e642eaabc4b6160f9e197c0db79d8b566074e0b2
Caties Mraz <zoucheng@icsoc.net>
2019-04-28 14:13:27 +0800

    【ADD】 添加layout模板 修改bookController.index

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,31 @@
-const Book = require('../models/book');
+var Book = require('../models/book');
+var Author = require('../models/author');
+var Genre = require('../models/genre');
+var BookInstance = require('../models/bookinstance');
 
-exports.index = (req, res) => { 
-  res.send('未实现：站点首页');
+var async = require('async');
+
+exports.index = function(req, res) {   
+    
+    async.parallel({
+        book_count: function(callback) {
+            Book.count({}, callback); // Pass an empty object as match condition to find all documents of this collection
+        },
+        book_instance_count: function(callback) {
+            BookInstance.count({}, callback);
+        },
+        book_instance_available_count: function(callback) {
+            BookInstance.count({status:'Available'}, callback);
+        },
+        author_count: function(callback) {
+            Author.count({}, callback);
+        },
+        genre_count: function(callback) {
+            Genre.count({}, callback);
+        },
+    }, function(err, results) {
+        res.render('index', { title: 'Local Library Home', error: err, data: results });
+    });
 };
 
 // 显示完整的藏书列表
