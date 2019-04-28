a6c6435a9a903dcae650422891b08157fe56d98b
Caties Mraz <zoucheng@icsoc.net>
2019-04-28 14:24:25 +0800

    【ADD】添加index template

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,7 @@
 var Book = require('../models/book');
 var Author = require('../models/author');
 var Genre = require('../models/genre');
-var BookInstance = require('../models/bookinstance');
+var BookInstance = require('../models/bookInstance');
 
 var async = require('async');
 
